6eb5b267eb8c7fcaea1e19c41fd9d5fb6927cd4c
markdefaoite <markdefaoite@hotmail.com>
2024-06-17 13:13:11 -0400

    Final product

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
 current_card = None
+to_learn = {}
 
 
 def next_card():
@@ -16,15 +17,32 @@ def next_card():
     flip_timer = window.after(3000, func=show_english_word)
 
 
+
 def show_english_word():
     global current_card
     canvas.itemconfig(canvas_image, image=card_back)
     canvas.itemconfig(card_title, text="English", fill="white")
     canvas.itemconfig(card_word, text=current_card["English"], fill="white")
 
+def is_known():
+    global current_card
+    to_learn.remove(current_card)
+    data = pandas.DataFrame(to_learn)
+    data.to_csv("data/words_to_learn.csv", index=False)
 
-data = pandas.read_csv("data/french_words.csv")
-to_learn = data.to_dict(orient="records")
+
+    next_card()
+
+
+try:
+    data = pandas.read_csv("data/words_to_learn.csv")
+except FileNotFoundError:
+    original_data = pandas.read_csv("data/french_words.csv")
+    to_learn = original_data.to_dict(orient="records")
+else:
+    to_learn = data.to_dict(orient="records")
+
+
 
 window = Tk()
 window.title("Flashy")
@@ -43,7 +61,7 @@ card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
 
 right_img = PhotoImage(file="images/right.png")
-known_button = Button(image=right_img, highlightthickness=0, command=next_card)
+known_button = Button(image=right_img, highlightthickness=0, command=is_known)
 known_button.grid(row=1, column=1)
 
 wrong_img = PhotoImage(file="images/wrong.png")
@@ -51,5 +69,7 @@ unknown_button = Button(image=wrong_img, highlightthickness=0, command=show_engl
 unknown_button.grid(row=1, column=0)
 
 next_card()
+
+
 
 window.mainloop()
