c3f29ea555ecc220fb6967be7cd33cd355477ef8
markdefaoite <markdefaoite@hotmail.com>
2024-06-17 12:48:59 -0400

    timer working

--- main.py
+++ main.py
@@ -3,11 +3,24 @@ from tkinter import *
 import pandas
 
 BACKGROUND_COLOR = "#B1DDC6"
+current_card = None
+
 
 def next_card():
+    global current_card, flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(to_learn)
-    canvas.itemconfig(card_title, text="French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(canvas_image, image=card_front)
+    canvas.itemconfig(card_title, text="French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    flip_timer = window.after(3000, func=show_english_word)
+
+
+def show_english_word():
+    global current_card
+    canvas.itemconfig(canvas_image, image=card_back)
+    canvas.itemconfig(card_title, text="English", fill="white")
+    canvas.itemconfig(card_word, text=current_card["English"], fill="white")
 
 
 data = pandas.read_csv("data/french_words.csv")
@@ -17,24 +30,24 @@ window = Tk()
 window.title("Flashy")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
+flip_timer = window.after(3000, func=show_english_word)
+
 card_back = PhotoImage(file="images/card_back.png")
 card_front = PhotoImage(file="images/card_front.png")
 
 canvas = Canvas(width=800, height=526)
-canvas.create_image(400, 263, image=card_front)
+canvas_image = canvas.create_image(400, 263, image=card_front)
 canvas.grid(column=0, row=0, columnspan=2)
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 card_title = canvas.create_text(400, 150, text="", font=("Ariel", 40, "italic"))
 card_word = canvas.create_text(400, 263, text="", font=("Ariel", 60, "bold"))
-
-
 
 right_img = PhotoImage(file="images/right.png")
 known_button = Button(image=right_img, highlightthickness=0, command=next_card)
 known_button.grid(row=1, column=1)
 
 wrong_img = PhotoImage(file="images/wrong.png")
-unknown_button = Button(image=wrong_img, highlightthickness=0, command=next_card)
+unknown_button = Button(image=wrong_img, highlightthickness=0, command=show_english_word)
 unknown_button.grid(row=1, column=0)
 
 next_card()
